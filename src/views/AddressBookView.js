ee2a24f33ad0de2168883270d3ed9f3b2b880887
piricf <piricf9@gmail.com>
2021-01-24 13:03:11 +0100

    wip

--- src/views/AddressBookView.js
+++ src/views/AddressBookView.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { logoutUser } from "../redux/auth/userAction";
 
 const AddressBookView = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const user = useSelector((state) => state.userReducer);
+
+  const logout = () => {
+    dispatch(logoutUser());
+  };
   return (
     <div>
       <h1>This is address book</h1>
