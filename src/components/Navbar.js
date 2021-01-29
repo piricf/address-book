6140ee8ca0b99acf0264feae6d52c01452d6da7b
piricf <piricf9@gmail.com>
2021-01-29 11:34:02 +0100

    navbar - fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,50 @@
-import React from "react";
-import { Input, Menu } from "semantic-ui-react";
+import React, { useEffect } from "react";
+import { Button, Input, Menu } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../redux/auth/userAction";
 
 const Navbar = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const { user } = useSelector((state) => state.userReducer);
 
   const logout = () => {
     dispatch(logoutUser());
   };
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+    }
+  }, [user, history]);
+
   return (
     <Menu secondary>
-      <Link to="/adresar">
-        <Menu.Item name="adresar">Adresar</Menu.Item>
-      </Link>
-      <Link to="/kontakti">
-        <Menu.Item name="kontakti">Kontakti</Menu.Item>
-      </Link>
+      {user ? (
+        <Menu.Menu position="left">
+          <Link to="/adresar">
+            <Menu.Item name="adresar">Adresar</Menu.Item>
+          </Link>
+          <Link to="/kontakti">
+            <Menu.Item name="kontakti">Kontakti</Menu.Item>
+          </Link>
+        </Menu.Menu>
+      ) : null}
       <Menu.Menu position="right">
         <Menu.Item>
           <Input icon="search" placeholder="Search..." />
         </Menu.Item>
         {user ? (
-          <Link to="/" onClick={logout}>
+          <Button to="/" onClick={logout}>
             <Menu.Item>Log Out</Menu.Item>
-          </Link>
+          </Button>
         ) : (
-          <Link to="/">
+          <Button to="/">
             <Menu.Item>Log In</Menu.Item>
-          </Link>
+          </Button>
         )}
       </Menu.Menu>
     </Menu>
